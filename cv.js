fce3e3e49ad1120d5e79c2508f07c5e4410b7304
Marina <marinafaggian@gmail.com>
2023-02-09 17:30:26 -0300

    agrego contenedor de listas cambio en css con respecto a las listas comienzo de escritura de js

---
+++ cv.js
@@ -0,0 +1,8 @@
+let listElements = document.querySelectorAll('fa-regular');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=> {
+        alert()
+    })
+
+});
