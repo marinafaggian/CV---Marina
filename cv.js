0f450c24a644b801ffc9cfdb80323c9f7c26ca98
Marina <marinafaggian@gmail.com>
2023-02-16 15:03:13 -0300

    Agrego botón de "Coreuta"

--- cv.js
+++ cv.js
@@ -6,8 +6,17 @@ window.addEventListener("load", () => {
     botonExperiencia.addEventListener("click", () => {
         contExperiencia.classList.toggle("height-0")
     });
-});
 
+
+    const contCoreuta = document.querySelector(".lista-coreuta");
+    console.log(contCoreuta);
+    const botonCoreuta = document.querySelector(".sub-cabezal.coreuta");
+    console.log(botonCoreuta);
+    botonCoreuta.addEventListener("click", () => {
+        contCoreuta.classList.toggle("height-0")
+    });
+    
+});
 
 
 
