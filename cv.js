078eab78232c9ce8eaa5a15055539db297131420
Marina <marinafaggian@gmail.com>
2023-02-16 15:25:40 -0300

    Agrego botones "Directora" y "docente"

--- cv.js
+++ cv.js
@@ -4,18 +4,34 @@ window.addEventListener("load", () => {
     const botonExperiencia = document.querySelector(".cabezal-boton.experiencia");
     console.log(botonExperiencia);
     botonExperiencia.addEventListener("click", () => {
-        contExperiencia.classList.toggle("height-0")
+        contExperiencia.classList.toggle("height-Exp")
     });
 
 
-    const contCoreuta = document.querySelector(".lista-coreuta");
+    const contCoreuta = document.querySelector(".lista.coreuta");
     console.log(contCoreuta);
     const botonCoreuta = document.querySelector(".sub-cabezal.coreuta");
     console.log(botonCoreuta);
     botonCoreuta.addEventListener("click", () => {
-        contCoreuta.classList.toggle("height-0")
+        contCoreuta.classList.toggle("height-lista")
     });
     
+    const contDirectora = document.querySelector(".lista.directora");
+    console.log(contDirectora);
+    const botonDirectora = document.querySelector(".sub-cabezal.directora");
+    console.log(botonDirectora);
+    botonDirectora.addEventListener("click", () => {
+        contDirectora.classList.toggle("height-lista")
+    });
+
+    const contDocente = document.querySelector(".lista.docente");
+    console.log(contDocente);
+    const botonDocente = document.querySelector(".sub-cabezal.docente");
+    console.log(botonDocente);
+    botonDocente.addEventListener("click", () => {
+        contDocente.classList.toggle("height-lista")
+    });
+
 });
 
 
