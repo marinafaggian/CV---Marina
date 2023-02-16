0aabcd4c3593f027287bea4d072cb2c2110c44da
Marcos Di Paolo <mdipaolo@nerdwallet.com>
2023-02-16 14:12:25 -0300

    commit Marcos

--- cv.js
+++ cv.js
@@ -1,6 +1,13 @@
-//intentando hacer que la flecha de despliegue funcione
+window.addEventListener("load", () => {
+    const contExperiencia = document.querySelector(".contenedor-seccion-experiencia");
+    console.log(contExperiencia);
+    const botonExperiencia = document.querySelector(".cabezal-boton.experiencia");
+    console.log(botonExperiencia);
+    botonExperiencia.addEventListener("click", () => {
+        contExperiencia.classList.toggle("height-0")
+    });
+});
 
-console.log("hola Mundo")
 
 
 
